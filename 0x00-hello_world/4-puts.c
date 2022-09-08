1ffd6387288550a523d834990aaeec6d2002a48e
‘candacebe’ <hindekie@gamil.com>
2022-09-08 05:34:52 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
 #include<stdio.h>
-/*** main - Entry point
+/** 
+* main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: 0 after printing the function
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
