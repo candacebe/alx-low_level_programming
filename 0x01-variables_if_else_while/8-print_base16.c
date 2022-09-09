a74438e09b3ace5d6e9d145bcc726db08d658336
‘candacebe’ <hindekie@gamil.com>
2022-09-09 08:54:28 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Prints numbers between 0 to 9 and letters between a to f.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(alp[i]);
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
